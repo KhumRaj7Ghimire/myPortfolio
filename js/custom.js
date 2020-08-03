bcf3950337638fdc59279a175592701ae90092af
Khum Raj Ghimire <khumraj7ghimire@gmail.com>
2020-08-03 16:28:37 +0545

    testing-aos9

--- js/custom.js
+++ js/custom.js
@@ -4,25 +4,6 @@ $('#year').text(new Date().getFullYear());
 // Init Scrollspy
 $('body').scrollspy({ target: '#main-nav' });
 
-// // Smooth Scrolling
-// $('#main-nav a').on('click', function (event) {
-//   if (this.hash !== '') {
-//     event.preventDefault();
-
-//     const hash = this.hash;
-
-//     $('html, body').animate(
-//       {
-//         scrollTop: $(hash).offset().top,
-//       },
-//       800,
-//       function () {
-//         window.location.hash = hash;
-//       }
-//     );
-//   }
-// });
-
 // NAVBAR TRANSSPARENT TO SOLID
 $(document).ready(function () {
   $(window).scroll(function () {
